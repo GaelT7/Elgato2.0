8edc2a9d7955b044a883a7c0d825f2fedd963c74
Jeraset17 <94151629+Jeraset17@users.noreply.github.com>
2022-11-12 09:59:50 -0600

    Update ElGato.c

--- ElGato.c
+++ ElGato.c
@@ -1,10 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
+    
+    char op;
     printf("EL JUEGO DEL GATO");
     printf("Se supone que seguiria programando aqui");
-
-
+    
+    do
+    {
+        printf("\n\nEL GATO\n");
+        printf("\t1. Jugar");
+        printf("\t2. Estadisticas");
+        printf("\t3. Salir");
+        printf("\tSeleccione una opcion: ");
+        fflush(stdin);
+        scanf(" %c",&op);
+        switch(op)
+        {
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                printf("\nPrograma terminado");
+                break;
+            default:
+                printf("\nOpcion invalida");
+        }
+    }while(op!='3');
+    
 
 
 
