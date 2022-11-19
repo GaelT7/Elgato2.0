dbf94ddb3fbe00883684e3e5aa0de4b3049c73ae
Jeraset17 <94151629+Jeraset17@users.noreply.github.com>
2022-11-19 16:24:52 -0600

    Update ElGato.c

--- ElGato.c
+++ ElGato.c
@@ -1,23 +1,113 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+struct jugador
+{
+    char icono,nombre[10];
+}Jugador_1,Jugador_2;
+
 int main(){
-    
-    char op;
-    printf("EL JUEGO DEL GATO");
-    printf("Se supone que seguiria programando aqui");
-    
+
+    char op,tablero[3][3];
+    int tablero_aux[3][3],pos=1,emp=0,casilla,cambio_jugador=0,band=0;
+    for(int i=0;i<3;i++)
+    {
+        printf("\n");
+        for(int j=0;j<3;j++)
+        {
+            tablero_aux[i][j] = pos;
+            pos++;
+            printf(" [%i]",tablero_aux[i][j]);
+        }
+    }
+    Jugador_1.icono='x';
+    Jugador_2.icono='o';
+    printf("\n\nJugador 1: %c",Jugador_1.icono);
+    printf("\n\nJugador 2: %c",Jugador_2.icono);
+
+    for(int i=0;i<3;i++)
+    {
+        printf("\n");
+        for(int j=0;j<3;j++)
+        {
+            tablero[i][j]=' ';
+            printf(" [%c]",tablero[i][j]);
+        }
+    }
+
     do
     {
         printf("\n\nEL GATO\n");
-        printf("\t1. Jugar");
-        printf("\t2. Estadisticas");
-        printf("\t3. Salir");
-        printf("\tSeleccione una opcion: ");
+        printf("1. Jugar\n");
+        printf("2. Estadisticas\n");
+        printf("3. Salir\n");
+        printf("Seleccione una opcion: ");
         fflush(stdin);
         scanf(" %c",&op);
+
         switch(op)
         {
             case '1':
+                do
+                {
+
+                    printf("\ningresa una casilla: ");
+                    fflush(stdin);
+                    scanf(" %i",&casilla);
+                    if(cambio_jugador == 0)
+                    {
+                        for(int i=0;i<3;i++)
+                        {
+                            printf("\n");
+                            for(int j=0;j<3;j++)
+                            {
+                                if(tablero_aux[i][j] == casilla)
+                                {
+                                    tablero[i][j] = Jugador_1.icono;
+                                    tablero_aux[i][j] = 0;
+                                    band=1;
+
+                                }
+
+                                printf(" [%c]",tablero[i][j]);
+
+                            }
+                        }
+                        if(band!=1)
+                        {
+                            printf("Casilla no encontrada");
+                            band=0;
+                        }
+                        cambio_jugador=1;
+                    }
+                    else
+                    {
+                        for(int i=0;i<3;i++)
+                        {
+                            printf("\n");
+                            for(int j=0;j<3;j++)
+                            {
+                                if(tablero_aux[i][j] == casilla)
+                                {
+                                    tablero[i][j] = Jugador_2.icono;
+                                    tablero_aux[i][j] = 0;
+                                    band=1;
+
+                                }
+
+                                printf(" [%c]",tablero[i][j]);
+
+                            }
+                        }
+                        if(band!=1)
+                        {
+                            printf("Casilla no encontrada");
+                            band=0;
+                        }
+                        cambio_jugador=0;
+                    }
+                    emp++;
+                }while(emp!=9);
                 break;
             case '2':
                 break;
@@ -28,7 +118,7 @@ int main(){
                 printf("\nOpcion invalida");
         }
     }while(op!='3');
-    
+
 
 
 
