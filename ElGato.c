f75985b82718714905d873711e8e6adb1ac90831
Jeraset17 <94151629+Jeraset17@users.noreply.github.com>
2022-11-20 19:00:12 -0600

    Update ElGato.c

--- ElGato.c
+++ ElGato.c
@@ -4,12 +4,14 @@
 struct jugador
 {
     char icono,nombre[10];
+    int casilla,ganar[9];
 }Jugador_1,Jugador_2;
 
 int main(){
 
     char op,tablero[3][3];
     int tablero_aux[3][3],pos=1,emp=0,casilla,cambio_jugador=0,band=0;
+
     for(int i=0;i<3;i++)
     {
         printf("\n");
@@ -23,7 +25,7 @@ int main(){
     Jugador_1.icono='x';
     Jugador_2.icono='o';
     printf("\n\nJugador 1: %c",Jugador_1.icono);
-    printf("\n\nJugador 2: %c",Jugador_2.icono);
+    printf("\n\nJugador 2: %c\n\n",Jugador_2.icono);
 
     for(int i=0;i<3;i++)
     {
@@ -50,64 +52,70 @@ int main(){
             case '1':
                 do
                 {
-
-                    printf("\ningresa una casilla: ");
-                    fflush(stdin);
-                    scanf(" %i",&casilla);
                     if(cambio_jugador == 0)
                     {
-                        for(int i=0;i<3;i++)
+                        do
                         {
-                            printf("\n");
-                            for(int j=0;j<3;j++)
+                            printf("\ningresa una casilla jugador 1: ");
+                            fflush(stdin);
+                            scanf(" %i",&Jugador_1.casilla);
+                            for(int i=0;i<3;i++)
                             {
-                                if(tablero_aux[i][j] == casilla)
+                                printf("\n");
+                                for(int j=0;j<3;j++)
                                 {
-                                    tablero[i][j] = Jugador_1.icono;
-                                    tablero_aux[i][j] = 0;
-                                    band=1;
-
+                                    if(tablero_aux[i][j] == Jugador_1.casilla)
+                                    {
+                                        tablero[i][j] = Jugador_1.icono;
+                                        tablero_aux[i][j] = -1;
+                                        band=1;
+                                    }
+                                    printf(" [%c]",tablero[i][j]);
                                 }
-
-                                printf(" [%c]",tablero[i][j]);
-
                             }
-                        }
-                        if(band!=1)
-                        {
-                            printf("Casilla no encontrada");
-                            band=0;
-                        }
+                            if(band==0)
+                            {
+                                printf("\n\nCasilla no encontrada\n");
+                            }
+                        }while(band!=1);
+                        band=0;
                         cambio_jugador=1;
                     }
                     else
                     {
-                        for(int i=0;i<3;i++)
+                        do
                         {
-                            printf("\n");
-                            for(int j=0;j<3;j++)
+                            printf("\ningresa una casilla jugador 2: ");
+                            fflush(stdin);
+                            scanf(" %i",&Jugador_2.casilla);
+                            for(int i=0;i<3;i++)
                             {
-                                if(tablero_aux[i][j] == casilla)
+                                printf("\n");
+                                for(int j=0;j<3;j++)
                                 {
-                                    tablero[i][j] = Jugador_2.icono;
-                                    tablero_aux[i][j] = 0;
-                                    band=1;
-
+                                    if(tablero_aux[i][j] == Jugador_2.casilla)
+                                    {
+                                        tablero[i][j] = Jugador_2.icono;
+                                        tablero_aux[i][j] = -1;
+                                        band=1;
+                                    }
+                                    printf(" [%c]",tablero[i][j]);
                                 }
-
-                                printf(" [%c]",tablero[i][j]);
-
                             }
-                        }
-                        if(band!=1)
-                        {
-                            printf("Casilla no encontrada");
-                            band=0;
-                        }
+                            if(band==0)
+                            {
+                                printf("\n\nCasilla no encontrada\n");
+                            }
+                        }while(band!=1);
+                        band=0;
                         cambio_jugador=0;
                     }
                     emp++;
+
                 }while(emp!=9);
+
+                if(emp==9)
+                    printf("\n\nEMPATE");
                 break;
             case '2':
                 break;
@@ -119,9 +127,6 @@ int main(){
         }
     }while(op!='3');
 
-
-
-
-
 return 0;
 }
+
